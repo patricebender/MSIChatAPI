e6400594ec4503b51315024505799b680a584452
Patrice Bender <info@patrice.codes>
2019-05-04 09:09:02 +0200

    add socket.io

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const http = require('http').Server(app);
+
+const io = require('socket.io')(http);
+
 
 app.get("/", (req,res) => {
 	res.send({hello: "World"});
+});
+
+io.on('connection', function(socket){
+  console.log('an user connected');
 });
 
 const PORT = process.env.PORT || 5000;
